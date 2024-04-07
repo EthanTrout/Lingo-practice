81478baf6f914fdd199357578fe83563911c3e31
EthanTrout <ethan-james-trout@outlook.com>
2024-04-07 12:02:23 +0100

    fix: Refactored displayAnswer, setTileGreen and setTile orange. using array instead of if statements

--- TESTING.md
+++ TESTING.md
@@ -89,3 +89,197 @@ expected results are first set of tiles. green green blank green
 
 expected result are second set of tiles. green green orange blank
 ![Result](/assets/testing-images/inputsTest2.png)
+
+## Incrementing rounds 
+
+### testing testing for 
+- after each guess the round increments 
+- after each round the correct letters are passed to the next tiles 
+- after each round orange tiles are not passed on 
+- after each round the input is reset 
+
+![Result 1](/assets/testing-images/roundsTest1.png)
+![Result 2](/assets/testing-images/roundsTest2.png)
+![Result 3](/assets/testing-images/roundsTest3.png)
+
+
+## Refactoring functions after testing 
+
+### displayResults
+
+After getting Display results functioning and tested i have refactored to code the use less lines. 
+
+#### changes:
+- each set of round tiles objects were previously stored as a variable.
+    - they are now set as a array
+- the displayResults function had multiple if statements checking what round it was and what variable to use.
+    - now the function uses the roundCounter as an index to which round tile object to use 
+
+
+#### Previous code 
+
+```javascript 
+const gameController ={
+    gameTimer:10,
+    lingoWord: "beet",
+    roundCounter:0,
+    round1Tiles: document.querySelectorAll(".round-1"),
+    round2Tiles: document.querySelectorAll(".round-2"),
+    round3Tiles: document.querySelectorAll(".round-3"),
+    round4Tiles: document.querySelectorAll(".round-4"),
+    round5Tiles: document.querySelectorAll(".round-5"),
+    userAnswer: document.getElementById("user-answer").value,
+    correctLetters:[]
+
+};
+
+function displayAnswer(){
+    if(gameController.roundCounter ===1){
+        for(x =0; x<gameController.round1Tiles.length;x++){
+            gameController.round1Tiles[x].innerText = gameController.userAnswer[x];
+        }
+    }
+    else if(gameController.roundCounter ===2){
+        for(x =0; x<gameController.round2Tiles.length;x++){
+            gameController.round2Tiles[x].innerText = gameController.userAnswer[x];
+        }
+    }
+    else if(gameController.roundCounter ===3){
+        for(x =0; x<gameController.round3Tiles.length;x++){
+            gameController.round3Tiles[x].innerText = gameController.userAnswer[x];
+        }
+    }
+    else if(gameController.roundCounter ===4){
+        for(x =0; x<gameController.round4Tiles.length;x++){
+            gameController.round4Tiles[x].innerText = gameController.userAnswer[x];
+        }
+    }
+    else if(gameController.roundCounter ===5){
+        for(x =0; x<gameController.round5Tiles.length;x++){
+            gameController.round5Tiles[x].innerText = gameController.userAnswer[x];
+        }
+    }
+}
+
+```
+
+#### Refactored code 
+
+```javascript 
+const gameController ={
+    gameTimer:10,
+    lingoWord: "beet",
+    roundCounter:0,
+    roundTiles:[document.querySelectorAll(".round-1"),document.querySelectorAll(".round-2"),document.querySelectorAll(".round-3"),document.querySelectorAll(".round-4"),document.querySelectorAll(".round-5")],
+    userAnswer: document.getElementById("user-answer").value,
+    correctLetters:[]
+
+};
+
+function displayAnswer(){
+    var roundIndex =gameController.roundCounter;
+    console.log(gameController.roundTiles[roundIndex])
+    for(x =0; x<gameController.roundTiles[roundIndex].length;x++){
+        gameController.roundTiles[roundIndex][x].innerText = gameController.userAnswer[x];
+    }
+    
+}
+```
+
+### SetTileGreen
+
+After getting setTileGreen functioning and tested i have refactored to code the use less lines.
+
+### changes
+- using the new index of roundTiles objects i removed all if statment
+    - they were replaced by using the roundCounter as an index.
+- an if statment was added to ensure on the fith round nothing was passed to the the 6th as it would not exisit 
+
+#### Previous Code
+
+```javascript 
+function setTileGreen (index){
+if(gameController.roundCounter ===0){
+    gameController.round1Tiles[index].style.backgroundColor = "green";
+    gameController.round2Tiles[index].innerText = gameController.userAnswer[index];
+    
+}
+else if (gameController.roundCounter ===1){
+    gameController.round2Tiles[index].style.backgroundColor = "green";
+    gameController.round3Tiles[index].innerText = gameController.userAnswer[index];
+    
+}
+else if (gameController.roundCounter ===2){
+    gameController.round3Tiles[index].style.backgroundColor = "green";
+    gameController.round4Tiles[index].innerText = gameController.userAnswer[index];
+    
+}
+else if (gameController.roundCounter ===3){
+    gameController.round4Tiles[index].style.backgroundColor = "green";
+    gameController.round5Tiles[index].innerText = gameController.userAnswer[index];
+    
+}
+else if (gameController.roundCounter ===4){
+    gameController.round5Tiles[index].style.backgroundColor = "green";
+}
+else{
+    
+}
+
+}
+```
+
+#### Refactored code
+
+```javascript 
+function setTileGreen (index){
+    var roundIndex = gameController.roundCounter;
+    gameController.roundTiles[roundIndex][index].style.backgroundColor ="green";
+    if(roundIndex != 4){
+        gameController.roundTiles[roundIndex+1][index].innerText = gameController.userAnswer[index];
+    }
+}
+```
+
+### setTileOrange 
+the same changes to setTileGreen were done to setTileOrange
+
+#### Previous code
+
+```javascript 
+function setTileOrange(index){
+    if(gameController.roundCounter ===0 && gameController.round1Tiles[index].style.backgroundColor != "green"){
+        gameController.round1Tiles[index].style.backgroundColor = "orange";
+    }
+    else if(gameController.roundCounter ===1 && gameController.round2Tiles[index].style.backgroundColor != "green"){
+        gameController.round2Tiles[index].style.backgroundColor = "orange";
+    }
+    else if(gameController.roundCounter ===2 && gameController.round3Tiles[index].style.backgroundColor != "green"){
+        gameController.round3Tiles[index].style.backgroundColor = "orange";
+    }
+    else if(gameController.roundCounter ===3 && gameController.round4Tiles[index].style.backgroundColor != "green"){
+        gameController.round4Tiles[index].style.backgroundColor = "orange";
+    }
+    else if(gameController.roundCounter ===4 && gameController.round5Tiles[index].style.backgroundColor != "green"){
+        gameController.round5Tiles[index].style.backgroundColor = "orange";
+    }
+    else{
+        
+    }
+}
+```
+
+#### Refactored code
+
+```javascript 
+function setTileOrange(index){
+    var roundIndex = gameController.roundCounter;
+    if(gameController.roundTiles[roundIndex][index].style.backgroundColor != "green"){
+        gameController.roundTiles[roundIndex][index].style.backgroundColor ="orange"
+    }
+}
+```
+
+
+
+
--- assets/js/lingogame.js
+++ assets/js/lingogame.js
@@ -62,17 +62,18 @@
 const gameController ={
     gameTimer:10,
     lingoWord: "beet",
-    roundCounter:1,
-    round1Tiles: document.querySelectorAll(".round-1"),
-    round2Tiles: document.querySelectorAll(".round-2"),
-    round3Tiles: document.querySelectorAll(".round-3"),
-    round4Tiles: document.querySelectorAll(".round-4"),
-    round5Tiles: document.querySelectorAll(".round-5"),
+    roundCounter:0,
+    roundTiles:[document.querySelectorAll(".round-1"),document.querySelectorAll(".round-2"),document.querySelectorAll(".round-3"),document.querySelectorAll(".round-4"),document.querySelectorAll(".round-5")],
+    // round1Tiles: document.querySelectorAll(".round-1"),
+    // round2Tiles: document.querySelectorAll(".round-2"),
+    // round3Tiles: document.querySelectorAll(".round-3"),
+    // round4Tiles: document.querySelectorAll(".round-4"),
+    // round5Tiles: document.querySelectorAll(".round-5"),
     userAnswer: document.getElementById("user-answer").value,
     correctLetters:[]
 
 };
-console.log(gameController.userAnswer);
+
 
 function submitAnswer(){
     if(gameController.gameTimer != 0){
@@ -90,7 +91,7 @@ function verifyAnswer(){
         win();
         
     }
-    else if(gameController.roundCounter === 6){
+    else if(gameController.roundCounter === 5){
         loose()
 
     }
@@ -118,98 +119,115 @@ function verifyAnswer(){
 
 function win(){
     // Set all tiles green//
-    gameController.roundCounter =1;
+    gameController.roundCounter =0;
     console.log("win")
 }
 
 function loose(){
     // Set all tiles red //
-    gameController.roundCounter =1;
+    gameController.roundCounter =0;
     console.log("loose")
 }
 
+// function displayAnswer(){
+//     if(gameController.roundCounter ===1){
+//         for(x =0; x<gameController.round1Tiles.length;x++){
+//             gameController.round1Tiles[x].innerText = gameController.userAnswer[x];
+//         }
+//     }
+//     else if(gameController.roundCounter ===2){
+//         for(x =0; x<gameController.round2Tiles.length;x++){
+//             gameController.round2Tiles[x].innerText = gameController.userAnswer[x];
+//         }
+//     }
+//     else if(gameController.roundCounter ===3){
+//         for(x =0; x<gameController.round3Tiles.length;x++){
+//             gameController.round3Tiles[x].innerText = gameController.userAnswer[x];
+//         }
+//     }
+//     else if(gameController.roundCounter ===4){
+//         for(x =0; x<gameController.round4Tiles.length;x++){
+//             gameController.round4Tiles[x].innerText = gameController.userAnswer[x];
+//         }
+//     }
+//     else if(gameController.roundCounter ===5){
+//         for(x =0; x<gameController.round5Tiles.length;x++){
+//             gameController.round5Tiles[x].innerText = gameController.userAnswer[x];
+//         }
+//     }
+// }
 function displayAnswer(){
-    if(gameController.roundCounter ===1){
-        for(x =0; x<gameController.round1Tiles.length;x++){
-            gameController.round1Tiles[x].innerText = gameController.userAnswer[x];
-        }
+    var roundIndex =gameController.roundCounter;
+    console.log(gameController.roundTiles[roundIndex])
+    for(x =0; x<gameController.roundTiles[roundIndex].length;x++){
+        gameController.roundTiles[roundIndex][x].innerText = gameController.userAnswer[x];
     }
-    else if(gameController.roundCounter ===2){
-        for(x =0; x<gameController.round2Tiles.length;x++){
-            gameController.round2Tiles[x].innerText = gameController.userAnswer[x];
-        }
-    }
-    else if(gameController.roundCounter ===3){
-        for(x =0; x<gameController.round3Tiles.length;x++){
-            gameController.round3Tiles[x].innerText = gameController.userAnswer[x];
-        }
-    }
-    else if(gameController.roundCounter ===4){
-        for(x =0; x<gameController.round4Tiles.length;x++){
-            gameController.round4Tiles[x].innerText = gameController.userAnswer[x];
-        }
-    }
-    else if(gameController.roundCounter ===5){
-        for(x =0; x<gameController.round5Tiles.length;x++){
-            gameController.round5Tiles[x].innerText = gameController.userAnswer[x];
-        }
-    }
-
-    
     
 }
 
 function setTileGreen (index){
-if(gameController.roundCounter ===1){
-    gameController.round1Tiles[index].style.backgroundColor = "green";
-    gameController.round2Tiles[index].innerText = gameController.userAnswer[index];
-    
-}
-else if (gameController.roundCounter ===2){
-    gameController.round2Tiles[index].style.backgroundColor = "green";
-    gameController.round3Tiles[index].innerText = gameController.userAnswer[index];
-    
-}
-else if (gameController.roundCounter ===3){
-    gameController.round3Tiles[index].style.backgroundColor = "green";
-    gameController.round4Tiles[index].innerText = gameController.userAnswer[index];
-    
-}
-else if (gameController.roundCounter ===4){
-    gameController.round4Tiles[index].style.backgroundColor = "green";
-    gameController.round5Tiles[index].innerText = gameController.userAnswer[index];
-    
-}
-else if (gameController.roundCounter ===5){
-    gameController.round5Tiles[index].style.backgroundColor = "green";
-}
-else{
-    console.log("no green")
+    var roundIndex = gameController.roundCounter;
+    gameController.roundTiles[roundIndex][index].style.backgroundColor ="green";
+    if(roundIndex != 4){
+        gameController.roundTiles[roundIndex+1][index].innerText = gameController.userAnswer[index];
+    }
 }
 
+// function setTileGreen (index){
+// if(gameController.roundCounter ===0){
+//     gameController.round1Tiles[index].style.backgroundColor = "green";
+//     gameController.round2Tiles[index].innerText = gameController.userAnswer[index];
+    
+// }
+// else if (gameController.roundCounter ===1){
+//     gameController.round2Tiles[index].style.backgroundColor = "green";
+//     gameController.round3Tiles[index].innerText = gameController.userAnswer[index];
+    
+// }
+// else if (gameController.roundCounter ===2){
+//     gameController.round3Tiles[index].style.backgroundColor = "green";
+//     gameController.round4Tiles[index].innerText = gameController.userAnswer[index];
+    
+// }
+// else if (gameController.roundCounter ===3){
+//     gameController.round4Tiles[index].style.backgroundColor = "green";
+//     gameController.round5Tiles[index].innerText = gameController.userAnswer[index];
+    
+// }
+// else if (gameController.roundCounter ===4){
+//     gameController.round5Tiles[index].style.backgroundColor = "green";
+// }
+// else{
+    
+// }
 
-
-}
-
+// }
 
 function setTileOrange(index){
-    if(gameController.roundCounter ===1 && gameController.round1Tiles[index].style.backgroundColor != "green"){
-        gameController.round1Tiles[index].style.backgroundColor = "orange";
-    }
-    else if(gameController.roundCounter ===2 && gameController.round2Tiles[index].style.backgroundColor != "green"){
-        gameController.round2Tiles[index].style.backgroundColor = "orange";
-    }
-    else if(gameController.roundCounter ===3 && gameController.round3Tiles[index].style.backgroundColor != "green"){
-        gameController.round3Tiles[index].style.backgroundColor = "orange";
-    }
-    else if(gameController.roundCounter ===4 && gameController.round4Tiles[index].style.backgroundColor != "green"){
-        gameController.round4Tiles[index].style.backgroundColor = "orange";
-    }
-    else if(gameController.roundCounter ===5 && gameController.round5Tiles[index].style.backgroundColor != "green"){
-        gameController.round5Tiles[index].style.backgroundColor = "orange";
-    }
-    else{
-        console.log("no orange")
+    var roundIndex = gameController.roundCounter;
+    if(gameController.roundTiles[roundIndex][index].style.backgroundColor != "green"){
+        gameController.roundTiles[roundIndex][index].style.backgroundColor ="orange"
     }
 }
+
+// function setTileOrange(index){
+//     if(gameController.roundCounter ===0 && gameController.round1Tiles[index].style.backgroundColor != "green"){
+//         gameController.round1Tiles[index].style.backgroundColor = "orange";
+//     }
+//     else if(gameController.roundCounter ===1 && gameController.round2Tiles[index].style.backgroundColor != "green"){
+//         gameController.round2Tiles[index].style.backgroundColor = "orange";
+//     }
+//     else if(gameController.roundCounter ===2 && gameController.round3Tiles[index].style.backgroundColor != "green"){
+//         gameController.round3Tiles[index].style.backgroundColor = "orange";
+//     }
+//     else if(gameController.roundCounter ===3 && gameController.round4Tiles[index].style.backgroundColor != "green"){
+//         gameController.round4Tiles[index].style.backgroundColor = "orange";
+//     }
+//     else if(gameController.roundCounter ===4 && gameController.round5Tiles[index].style.backgroundColor != "green"){
+//         gameController.round5Tiles[index].style.backgroundColor = "orange";
+//     }
+//     else{
+        
+//     }
+// }
 
