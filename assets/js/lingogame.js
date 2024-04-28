210d93a9a633e64b0ef2f89732dffc799d52fca3
EthanTrout <ethan-james-trout@outlook.com>
2024-04-28 13:07:51 +0100

    fix: changed order of html to make it more readable

--- assets/js/lingogame.js
+++ assets/js/lingogame.js
@@ -479,7 +479,7 @@ const gameController ={
     isGrandPrize:false,
     isChoiceMade:false,
     isTimedGame:false,
-    LingoRoundStage:8,
+    LingoRoundStage:6,
     letterDisplayDelay:300,
     gameRoundDisplayDelay:3000
     
